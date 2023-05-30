2e55d73dcb70dc3ee196c87bab6c46a1fd5068e2
justinpsiegel <justinpsiegel@gmail.com>
2023-05-30 14:34:15 -0400

    Completed initial setup of portfolio and projects

---
+++ src/components/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1300px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-[#5EE3C0]">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1300px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>
+              I'm Justin, nice to meet you. Please take a look around to see all
+              of my experience and projects to date.
+            </p>
+          </div>
+          <div>
+            <p>
+              After working for nearly a decade as an advertising professional,
+              I'm seeking a new challenge in programming. My passion for logic
+              and problem-solving has lead me to seek out something further in
+              line with what excites me. I love growing relationships in a
+              professional environment and I want to work with a team to create
+              great things. I love the challenge of learning programming
+              languages as well as solving an interesting logic puzzle. While I
+              feel as though the advertising field could never quite fulfill me
+              in that regard, I'm eager to get my software engineering career
+              under way so I can scratch those itches while being part of a
+              team!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/a4717756-a1d6-412a-8109-f523c77eef7f"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#5EE3C0] text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            \\ Submit the form below or shoot me an email
+          </p>
+        </div>
+        <input
+          className="p-2 bg-[#ccd6f6]"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="text-white border-2 hover:bg-[#5EE3C0] hover:border-[#5EE3C0] px-4 py-3 my-8 mx-auto flex items-center">
+          Let's Collaborate
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
