7418a93b61ab988ec552785fe8450c3df573ab1a
justinpsiegel <justinpsiegel@gmail.com>
2023-06-26 17:26:05 -0400

    Added eCommerce Project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Venari from "../assets/projects/venari.png";
 import adminDashboard from "../assets/projects/adminDashboard.JPG";
 import typescriptQuiz from "../assets/projects/quizScreenshot.JPG";
+import eCommerce from "../assets/projects/eCommerce.JPG";
 
 const Work = () => {
   return (
@@ -28,6 +29,28 @@ const Work = () => {
                   rel="noreferrer"
                   target="_blank"
                   href="https://venari.onrender.com/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    See Project
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${eCommerce})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Shop of The Gull
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://e-commerce-nu-blush.vercel.app/"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     See Project
