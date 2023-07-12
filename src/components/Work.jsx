0c33eee3e6aebad48ed01e76039124bc23d35d4b
justinpsiegel <justinpsiegel@gmail.com>
2023-07-11 20:10:51 -0400

    Fixed section height issue

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ import eCommerce from "../assets/projects/eCommerce.JPG";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-[40vh] text-gray-300 bg-[#0a192f]">
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
       <div className="max-w-[1300px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#5EE3C0]">
